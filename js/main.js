f4ec869bf7e7ffba8bcd231aa8d42cafebec10f8
Lucian Andercou <lucian.andercou@gmail.com>
2017-03-21 20:39:13 +0200

    Approximative layout of login box

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-var app = angular.module('office',[]);
+var app = angular.module('office',['ng.shims.placeholder','jcs-autoValidate']); /*might need to downgrade to some lower version of angular*/
+
+app.controller('MainController',function($scope){
+
+    $scope.onSubmit = function(valid){
+        if(valid)
+            {
+                console.log("It has been submitted");
+            } else{
+                console.log("Invalid form !");
+            }
+    }
+});
